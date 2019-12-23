ef064bd1a003f87c8f7b2d9a1a55c8004ea82600
Hiromu OCHIAI <otiai10@gmail.com>
2019-12-23 12:58:36 +0900

    Use yml for ESLint

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": 0
-  }
-}
